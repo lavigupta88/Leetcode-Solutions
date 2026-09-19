f27b25fb65cf3a2296f955bb48d75d6d755d2eda
lavi gupta <lavigupta8814@gmail.com>
2026-09-19 16:17:54 +0530

    Time: 1 ms (95.99%), Space: 47.7 MB (24.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,51 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length; //rows
+        int n = matrix[0].length; //column
 
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
 
-        for(int i =0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    row [i] = true;
-                    col [j] = true;
+        for(int j = 0; j<n; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i<m; i++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix [i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for(int i =1; i<m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int i =0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(row[i] || col[j]){
-                    matrix[i][j] = 0;
-                }
+        if(firstRowZero){
+            for(int j = 0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColZero){
+            for(int i = 0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
     }
